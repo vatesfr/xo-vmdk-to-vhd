b38e62a4ffed84eb75c004d25b2d14f74c198a87
Nicolas Raynaud <github@nraynaud.com>
2016-08-10 13:40:25 +0200

    virtual buffer: add isDepleted() and use async/await in tests

--- test/virtual-buffer-test.js
+++ test/virtual-buffer-test.js
@@ -7,16 +7,13 @@ import {createReadStream, readFile} from 'fs-promise'
 import {VirtualBuffer} from '../src/virtual-buffer'
 
 describe('Virtual Buffer', function () {
-  it('readRawContent() can retrieve a random data file', () => {
+  it('can read a file correctly', async () => {
     let rawFileName = 'random-data'
-    return exec('base64 /dev/urandom | head -c 104448 > ' + rawFileName)
-      .then(() => {
-        const buffer = new VirtualBuffer(createReadStream(rawFileName))
-        return Promise.all([readFile(rawFileName), buffer.readChunk(0, 10), buffer.readChunk(10, -1)])
-      })
-      .then((array) => {
-        const original = array.shift()
-        assert.equal(Buffer.concat(array).toString('ascii'), original.toString('ascii'))
-      })
+    await exec('base64 /dev/urandom | head -c 104448 > ' + rawFileName)
+    const buffer = new VirtualBuffer(createReadStream(rawFileName))
+    const array = await Promise.all([readFile(rawFileName), buffer.readChunk(0, 10), buffer.readChunk(10, -1)])
+    const original = array.shift()
+    assert.equal(Buffer.concat(array).toString('ascii'), original.toString('ascii'))
+    assert(buffer.isDepleted, 'virtual buffer is depleted');
   })
 })
